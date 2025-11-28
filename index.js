37584f0ae21e3a3a01600b5317a2620b044f18f7
GabrielFagundes18 <gabrielfadundesvv@gamil.com>
2025-11-27 22:39:03 -0300

    git push -u origin main

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("ola Mundão")
+let nome = "Gabriel";
+let xp = "10001";
+let rank = "";
+
+if (xp <= 1000) {
+  rank = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  rank = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  rank = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+  rank = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  rank = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  rank = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  rank = "Imortal";
+} else {
+  rank = "Radiante";
+}
+
+console.log(" Seu nome é " + nome + " seu rank e " + rank);
