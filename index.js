7b2900d32e8bf6fd3a20c0f4077e07f1c5c62f69
GabrielFagundes18 <gabrielfadundesvv@gamil.com>
2025-11-27 23:38:51 -0300

    refatorar código para exibir nomes e ranks de heróis com base em suas experiências

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-let nome = "Gabriel";
-let xp = "10001";
-let rank = "";
+let nomeHerois = [
+  "Homem de Ferro",
+  "Capitã Marvel",
+  "Thor",
+  "Hulk",
+  "Viúva Negra",
+  "Pantera Negra",
+  "Doutor Estranho",
+  "Visão",
+  "Falcão",
+  "Máquina de Combate",
+  "Homem-Aranha",
+  "Groot",
+  "Rocket Raccoon",
+  "Deadpool",
+];
+let xpHeroi = [
+  950, 1500, 3200, 4800, 5500, 6900, 7100, 7950, 8400, 8999, 9200, 9800, 10001,
+  15000,
+];
+for (let i = 0; i < nomeHerois.length; i++) {
+  let nome = nomeHerois[i];
+  let xp = xpHeroi[i];
+  let rank = "";
 
-if (xp <= 1000) {
-  rank = "Ferro";
-} else if (xp >= 1001 && xp <= 2000) {
-  rank = "Bronze";
-} else if (xp >= 2001 && xp <= 5000) {
-  rank = "Prata";
-} else if (xp >= 6001 && xp <= 7000) {
-  rank = "Ouro";
-} else if (xp >= 7001 && xp <= 8000) {
-  rank = "Platina";
-} else if (xp >= 8001 && xp <= 9000) {
-  rank = "Ascendente";
-} else if (xp >= 9001 && xp <= 10000) {
-  rank = "Imortal";
-} else {
-  rank = "Radiante";
+  if (xp <= 1000) {
+    rank = "Ferro";
+  } else if (xp >= 1001 && xp <= 2000) {
+    rank = "Bronze";
+  } else if (xp >= 2001 && xp <= 5000) {
+    rank = "Prata";
+  } else if (xp >= 6001 && xp <= 7000) {
+    rank = "Ouro";
+  } else if (xp >= 7001 && xp <= 8000) {
+    rank = "Platina";
+  } else if (xp >= 8001 && xp <= 9000) {
+    rank = "Ascendente";
+  } else if (xp >= 9001 && xp <= 10000) {
+    rank = "Imortal";
+  } else {
+    rank = "Radiante";
+  }
+
+  console.log(`Seu nome é ${nome} seu rank e  ${rank}`);
 }
-
-console.log(" Seu nome é " + nome + " seu rank e " + rank);
